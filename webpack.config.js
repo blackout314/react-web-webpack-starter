4589f7668a267da2c85ea1e956b3f82fa7acfa10
francesco-strazzullo <me@francescostrazzullo.info>
2016-01-18 10:54:20 +0100

    added react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ var webpack = require("webpack");
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './src/index' // Your appʼs entry point
   ],
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, './build'),
     publicPath: '/build/',
     filename: "bundle.js"
   },
@@ -23,7 +24,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel']
+        loaders: ['react-hot', 'babel']
       }
     ]
   }
