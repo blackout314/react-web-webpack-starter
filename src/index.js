78be7fe00530a7c3efd6c95cf4d625a8a3e44edb
francesco-strazzullo <me@francescostrazzullo.info>
2016-07-13 15:36:10 +0200

    moved App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App/App';
+import App from './components/App';
 import { Router, Route, Link, browserHistory } from 'react-router';
  
 const element = (
